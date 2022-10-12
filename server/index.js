d2092cfc0a62852b43ec1563a310e510d62cdd2d
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-10-12 22:00:10 +0800

    add mongodb connection

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const port = process.env.PORT || 6000;
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect to database
+connectDB();
 
 app.use(
   "/graphql",
