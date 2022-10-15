b1b5c991c62599567f6a73f470b254f14fb4821d
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-10-15 12:44:28 +0800

    add project page

---
+++ client/src/pages/Project.jsx
@@ -0,0 +1,17 @@
+import { Link, useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECT } from "../queries/projectQueries";
+
+const Project = () => {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, {
+    variables: { id },
+  });
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Something Went Wrong</p>;
+  return <div>{data.project.name}</div>;
+};
+
+export default Project;
