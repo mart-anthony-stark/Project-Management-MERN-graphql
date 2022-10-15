e27be0ea6303a944d867ad838d8ce12be19c8262
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-10-15 15:48:29 +0800

    add client info

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
+import ClientInfo from "../components/ClientInfo";
 
 const Project = () => {
   const { id } = useParams();
@@ -11,7 +12,25 @@ const Project = () => {
 
   if (loading) return <Spinner />;
   if (error) return <p>Something Went Wrong</p>;
-  return <div>{data.project.name}</div>;
+
+  return (
+    <div>
+      {!loading && !error && (
+        <div className="mx-auto w-75 card p-5">
+          <Link to="/" className="btn btn-sm btn-light w-25 d-inline ms-auto">
+            Back
+          </Link>
+
+          <h1>{data.project.name}</h1>
+          <p>{data.project.description}</p>
+          <h5 className="mt-3">Project Status</h5>
+          <p className="lead">{data.project.status}</p>
+
+          <ClientInfo client={data.project.client} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Project;
